7b8137e762e466ca3ac2ef506db0e2909ad8a0b0
Leal31 <lealemanuel31@protonmail.com>
2021-11-03 15:02:33 -0500

    login

--- login/views.py
+++ login/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import Usuario
 # Create your views here.
 def main(request):
-
     return render(request, 'login/index.html')
+
+
+def Autenticar(request):
+
+        usuario = request.POST.get("usuario")
+        contraseña = request.POST.get("password")
+        str(contraseña)
+        autenticar = Usuario.objects.filter(Usuario=usuario, Contraseña=contraseña)
+        if autenticar == None:
+            return HttpResponse("No se encontro el usuario")
+        else:
+            return HttpResponse("Ingreso exitosamente al sistema")
