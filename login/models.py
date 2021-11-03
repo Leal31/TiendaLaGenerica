c142babd5fea69e3adafe26f51c27eca701f1d46
Leal31 <lealemanuel31@protonmail.com>
2021-11-03 14:38:22 -0500

    modelo

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+
+    Usuario=models.CharField(max_length=255, null=True)
+
+    Contraseña=models.CharField(max_length=255, null=True)
