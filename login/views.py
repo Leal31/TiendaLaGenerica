81dfec179caf514c181a973dfbcee06e524c534c
Leal31 <lealemanuel31@protonmail.com>
2021-11-05 12:12:50 -0500

    t

--- login/views.py
+++ login/views.py
@@ -2,24 +2,20 @@ from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseRedirect
 from .models import Usuario
 from django.core.exceptions import *
-from principal.views import tienda
 # Create your views here.
 def main(request):
-    return render(request, 'login/index.html')
+    autenticar = True
+    return render(request, 'login/index.html', {'autenticar' : autenticar})
 
 
 def Autenticar(request):
-    try:
-
-        usuario = request.POST.get("usuario")
-        contraseña = request.POST.get("password")
-        str(contraseña)
-        autenticar = Usuario.objects.filter(Usuario=usuario, Contraseña=contraseña).exists()
-        if autenticar:
-            autenticado = Usuario.objects.get(Usuario=usuario, Contraseña=contraseña)
-            return HttpResponseRedirect('tienda')
-        else:
-            return HttpResponseRedirect('/')
-
-    except ObjectDoesNotExist:
-        return HttpResponseRedirect('/')
+        if request.method == 'POST':
+            usuario = request.POST.get("usuario")
+            contraseña = request.POST.get("password")
+            str(contraseña)
+            autenticar = Usuario.objects.filter(Usuario=usuario, Contraseña=contraseña).exists()
+            if autenticar:
+                return HttpResponseRedirect('tienda')
+            else:
+                autenticar = False
+                return render(request, 'login/index.html', {'autenticar' : autenticar})
