b1a3c53e5c678657317b1c7af4a1a7d80f959d0f
Leal31 <lealemanuel31@protonmail.com>
2021-11-09 17:37:33 +0000

    metodos crud

--- productos/urls.py
+++ productos/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .api import router
 urlpatterns = [
     path('', views.productos, name = 'productos'),
     path('leercsv', views.cargar, name='leer'),
     path('importar', views.importar, name='importar'),
-    path('ProductoCrud', views.Producto_CRUD.as_view()),
-    path('ProductoCrud/<int:pk>/', views.Producto_Vista.as_view()),
+    path('productoCrud', include(router.urls)),
 ]
