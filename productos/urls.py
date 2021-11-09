7a4097f055aa4c0d83027ecccbf500c61bdfc8a4
Leal31 <lealemanuel31@protonmail.com>
2021-11-09 13:42:48 -0500

    c

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.productos, name = 'productos'),
-    path('/leercsv', views.cargar, name='leer'),
-    path('/importar', views.importar, name='importar'),
+    path('leercsv', views.cargar, name='leer'),
+    path('importar', views.importar, name='importar'),
 ]
