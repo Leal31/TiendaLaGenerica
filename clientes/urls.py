37b52ca2e05205829447d76aaaf25fa1e9a1cc8e
diego150104 <diegofernando150104@gmail.com>
2021-11-11 12:03:36 -0500

    solicitud

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('',views.tienda, name= 'clientes')
+    path('',views.tienda, name= 'clientes'),
+    path('consultar/<int:cedula>',views.consultar, name = 'consultar'),
+
 
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests 
 
 # Create your views here.
 def tienda(request):
-    return render(request,'cliente.html')
+    data=None
+    return render(request,'cliente.html',{"data":data})
+def consultar(request,cedula):
+    cedula = request.POST.get("cedula_cliente")
+    response= requests.get('localhost:8000/api/clientes/',cedula)
+    data=response.json()
+    return render (request, "cliente.html", {"data":data})
