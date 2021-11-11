072cc64312946cd4d15cf4127a3fd31237b00776
diego150104 <diegofernando150104@gmail.com>
2021-11-11 13:39:16 -0500

    crear

--- clientes/views.py
+++ clientes/views.py
@@ -13,3 +13,12 @@ def consultar(request):
     return render(request, "cliente.html", {"data":data})
 
 def crear(request):
+    nombre = request.POST.get('nombre_cliente')
+    cedula = request.POST.get('cedula_cliente')
+    telefono = request.POST.get('telefono_cliente')
+    email= request.POST.get('email_cliente')
+    direccion =request.POST.get('direccion_cliente')
+    dato={"cedula": cedula,"nombre": nombre, "telefono":telefono, "email":email,"direccion":direccion}
+    response =requests.post("http://localhost:8000/api/clientes/",data=dato)
+    data=None
+    return render(request, "cliente.html", {"data":data})
