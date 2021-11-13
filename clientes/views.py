b111feca74ce75b5c4a67676bcc86a668eab8b97
Leal31 <lealemanuel31@protonmail.com>
2021-11-13 13:03:35 +0000

    mensajes

--- clientes/views.py
+++ clientes/views.py
@@ -8,10 +8,16 @@ def tienda(request):
     return render(request,'clientes/cliente.html',{"data":data, 'mensaje':mensaje})
 
 def consultar(request):
+
     cedula = int(request.POST.get("cedula_cliente"))
     response= requests.get(f'http://localhost:8002/api/clientes/{cedula}/')
-    data=response.json()
-    mensaje = None
+    if response.status_code == 200:
+        data=response.json()
+        
+        mensaje = None
+    elif response.status_code == 404:
+        data = None
+        mensaje = "El cliente no existe"
     return render(request, "clientes/cliente.html", {"data":data, 'mensaje' : mensaje})
 
 def crear(request):
@@ -20,10 +26,15 @@ def crear(request):
     telefono = request.POST.get('telefono_cliente')
     email= request.POST.get('email_cliente')
     direccion =request.POST.get('direccion_cliente')
-    dato={"cedula": cedula,"nombre": nombre, "telefono":telefono, "email":email,"direccion":direccion}
-    response =requests.post("http://localhost:8002/api/clientes/",data=dato)
-    data=None
-    mensaje = "El cliente fue agregado correctamente"
+    response = requests.get(f'http://localhost:8002/api/clientes/{cedula}/')
+    if response.status_code == 404:
+        dato={"cedula": cedula,"nombre": nombre, "telefono":telefono, "email":email,"direccion":direccion}
+        response =requests.post("http://localhost:8002/api/clientes/",data=dato)
+        data=None
+        mensaje = "El cliente fue agregado correctamente"
+    elif response.status_code == 200:
+        data = None
+        mensaje = f"El cliente con la cedula {cedula} ya existe"
     return render(request, "clientes/cliente.html", {"data":data, 'mensaje' : mensaje})
 
 def actualizar(request):
@@ -32,15 +43,27 @@ def actualizar(request):
     telefono = request.POST.get('telefono_cliente')
     email= request.POST.get('email_cliente')
     direccion =request.POST.get('direccion_cliente')
-    dato={"cedula": cedula,"nombre": nombre, "telefono":telefono, "email":email,"direccion":direccion}
-    response=requests.put(f'http://localhost:8002/api/clientes/{cedula}/',data=dato)
-    data=None
-    mensaje = "El cliente fue actualizado satisfactoriamente"
+    response = requests.get(f'http://localhost:8002/api/clientes/{cedula}/')
+    if response.status_code == 200:
+
+        dato={"cedula": cedula,"nombre": nombre, "telefono":telefono, "email":email,"direccion":direccion}
+
+        response=requests.put(f'http://localhost:8002/api/clientes/{cedula}/',data=dato)
+        data=None
+        mensaje = "El cliente fue actualizado satisfactoriamente"
+    elif response.status_code == 404:
+        data = None
+        mensaje = f"El cliente con la cedula {cedula} no existe"
     return render(request, "clientes/cliente.html", {"data":data, 'mensaje' : mensaje})
 
 def borrar(request):
     cedula = int(request.POST.get("cedula_cliente"))
-    response= requests.delete(f'http://localhost:8002/api/clientes/{cedula}/')
-    data=None
-    mensaje = "El cliente fue eliminado satisfactoriamente"
+    response = requests.get(f'http://localhost:8002/api/clientes/{cedula}/')
+    if response.status_code == 200:
+        response= requests.delete(f'http://localhost:8002/api/clientes/{cedula}/')
+        data=None
+        mensaje = "El cliente fue eliminado satisfactoriamente"
+    elif response.status_code == 404:
+        data = None
+        mensaje = f"El cliente con la cedula {cedula} no existe"
     return render(request, "clientes/cliente.html", {"data":data, 'mensaje' : mensaje})
--- proveedores/views.py
+++ proveedores/views.py
@@ -9,20 +9,30 @@ def proveedores(request):
 def consultar(request):
     nit = int(request.POST.get('nit_pro'))
     response = requests.get(f'http://localhost:8003/api/proveedores/{nit}/')
-    data = response.json()
-    mensaje = None
+    if response.status_code == 200:
+
+        data = response.json()
+        mensaje = None
+    elif response.status_code == 404:
+        data = None
+        mensaje = "El proveedor no existe"
     return render(request, 'proveedores/proveedores.html', {'data' : data, 'mensaje' : mensaje})
 
 def crear(request):
     nit = int(request.POST.get('nit_pro'))
-    nombre = request.POST.get('nombre_pro')
-    direccion = request.POST.get('direccion_pro')
-    telefono = request.POST.get('telefono_pro')
-    ciudad = request.POST.get('ciudad_pro')
-    dato = {'Nit' : nit, 'nombre_proveedor' : nombre, 'direccion_proveedor' : direccion, 'telefono_proveedor' : telefono, 'ciudad_proveedor' : ciudad}
-    response = requests.post('http://localhost:8003/api/proveedores/', data=dato)
-    data = None
-    mensaje = "El proveedor fue creado satisfactoriamente"
+    response = requests.get(f'http://localhost:8003/api/proveedores/{nit}/')
+    if response.status_code == 404:
+        nombre = request.POST.get('nombre_pro')
+        direccion = request.POST.get('direccion_pro')
+        telefono = request.POST.get('telefono_pro')
+        ciudad = request.POST.get('ciudad_pro')
+        dato = {'Nit' : nit, 'nombre_proveedor' : nombre, 'direccion_proveedor' : direccion, 'telefono_proveedor' : telefono, 'ciudad_proveedor' : ciudad}
+        response = requests.post('http://localhost:8003/api/proveedores/', data=dato)
+        data = None
+        mensaje = "El proveedor fue creado satisfactoriamente"
+    elif response.status_code == 200:
+        data = None
+        mensaje = f"El proveedor con el nit {nit} ya existe"
     return render(request, 'proveedores/proveedores.html', {'data' : data, 'mensaje' : mensaje})
 
 def actualizar(request):
@@ -32,14 +42,24 @@ def actualizar(request):
     telefono = request.POST.get('telefono_pro')
     ciudad = request.POST.get('ciudad_pro')
     dato = {'Nit' : nit, 'nombre_proveedor' : nombre, 'direccion_proveedor' : direccion, 'telefono_proveedor' : telefono, 'ciudad_proveedor' : ciudad}
-    response = requests.put(f'http://localhost:8003/api/proveedores/{nit}/', data=dato)
-    data = None
-    mensaje = "El proveedor fue actualizado satisfactoriamente"
+    response = requests.get(f'http://localhost:8003/api/proveedores/{nit}/')
+    if response.status_code == 200:
+        response = requests.put(f'http://localhost:8003/api/proveedores/{nit}/', data=dato)
+        data = None
+        mensaje = "El proveedor fue actualizado satisfactoriamente"
+    elif response.status_code == 404:
+        data = None
+        mensaje = f"El proveedor con el nit {nit} no existe"
     return render(request, 'proveedores/proveedores.html', {'data' : data, 'mensaje' : mensaje})
 
 def borrar(request):
     nit = int(request.POST.get('nit_pro'))
     response = requests.delete(f'http://localhost:8003/api/proveedores/{nit}/')
-    data = None
-    mensaje = "El proveedor fue eliminado satisfactoriamente"
+    if response.status_code == 200:
+        response = requests.delete(f'http://localhost:8003/api/proveedores/{nit}/')
+        data = None
+        mensaje = "El proveedor fue eliminado satisfactoriamente"
+    elif response.status_code == 404:
+        data = None
+        mensaje = f"El proveedor con el nit {nit} no existe"
     return render(request, 'proveedores/proveedores.html', {'data' : data, 'mensaje' : mensaje})
