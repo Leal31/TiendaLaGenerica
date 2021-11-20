6fc693b590fb815f5995c14ed264785fe597cd57
Leal31 <lealemanuel31@protonmail.com>
2021-11-20 11:38:12 +0000

    venta

--- ventas/urls.py
+++ ventas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ventas, name = 'ventas'),
-    path('consultarCliente/', views.consultarCliente, name = 'consultarCliente'),
-    path('consultarProductos/', views.consultarProductos, name = 'consultarProductos'),
+    path('consultas/', views.consultarTodo, name = 'consultarTodo'),
+    path('subirVenta', views.subirVenta, name = 'subirVenta'),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -6,7 +6,8 @@ def ventas(request):
     Mensaje = None
     return render(request, 'ventas/ventas.html', {'Cliente' : data})
 
-def consultarCliente(request):
+
+def consultarTodo(request):
     try:
         cedula = request.POST.get('cedula_cliente')
     except Exception:
@@ -22,9 +23,7 @@ def consultarCliente(request):
         data = None
         mensaje = "No se encontro el cliente"
 
-    return render(request, 'ventas/ventas.html', {'Cliente' : data})
-
-def consultarProductos(request):
+   
     try:
         producto1 = request.POST.get('codigo_producto')
         producto2 = request.POST.get('codigo_producto2')
@@ -34,13 +33,14 @@ def consultarProductos(request):
         cantidad3 = request.POST.get('cantidad3')
     except Exception:
         print('algo ha salido mal')
+    
     if (producto1 == '' and producto2 == '') and producto3 == '':
         data1 = None
         data2 = None
         data3 = None
         mensaje = "No hay ningun producto ingresado"
         condicional = "todovacio"
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'condicional' : condicional})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'condicional' : condicional, 'Cliente':data})
     elif producto1 == '' and producto2 == '':
         mensaje = None
         condicional = "lleno3"
@@ -48,7 +48,7 @@ def consultarProductos(request):
         data1=None
         data2=None
         data3= response.json()
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad3' : cantidad3})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad3' : cantidad3, 'Cliente' : data})
     elif producto2 == '' and producto3 == '':
         mensaje = None
         condicional = "lleno1"
@@ -56,7 +56,7 @@ def consultarProductos(request):
         data1=response.json()
         data2=None
         data3=None
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad1' : cantidad1})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad1' : cantidad1, 'Cliente': data})
     elif producto1 == '' and producto3 == '':
         mensaje = None
         condicional = "lleno2"
@@ -64,7 +64,7 @@ def consultarProductos(request):
         data1=None
         data2= response.json()
         data3=None
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad2': cantidad2})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad2': cantidad2, 'Cliente':data})
     elif producto1 == '':
         mensaje = None
         condicional = "vacio1"
@@ -73,7 +73,7 @@ def consultarProductos(request):
         data1=None
         data2=response.json()
         data3=response1.json()
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional,'cantidad2': cantidad2,'cantidad3': cantidad3})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional,'cantidad2': cantidad2,'cantidad3': cantidad3, 'Cliente' : data})
     elif producto2 == '':
         mensaje = None
         condicional = "vacio2"
@@ -82,7 +82,7 @@ def consultarProductos(request):
         data1=response.json()
         data2=None
         data3=response1.json()
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional,'cantidad1': cantidad1,'cantidad3': cantidad3})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional,'cantidad1': cantidad1,'cantidad3': cantidad3, 'Cliente' : data})
     elif producto3 == '':
         mensaje = None
         condicional = "vacio3"
@@ -91,7 +91,7 @@ def consultarProductos(request):
         data1=response.json()
         data2=response1.json()
         data3=None
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional,'cantidad1': cantidad1,'cantidad2': cantidad2})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional,'cantidad1': cantidad1,'cantidad2': cantidad2, 'Cliente' : data})
     elif (producto1 != None and producto2!=None)and producto3!=None:
         mensaje = None
         condicional ="todosllenos"
@@ -101,11 +101,34 @@ def consultarProductos(request):
         data1=response.json()
         data2=response1.json()
         data3=response2.json()
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad1' : cantidad1, 'cantidad2' : cantidad2, 'cantidad3' : cantidad3})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'cantidad1' : cantidad1, 'cantidad2' : cantidad2, 'cantidad3' : cantidad3, 'Cliente' : data})
     else:
         mensaje= None
         condicional = None
         data1=None
         data2=None
         data3=None
-        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional})
+        return render(request, 'ventas/ventas.html', {'mensaje' : mensaje, 'data1' : data1, 'data2' : data2, 'data3': data3, 'condicional' : condicional, 'Cliente': data})
+
+def subirVenta(request):
+    try:
+        cedula_cliente=request.POST.get('cedula_cliente')
+        nombre_cliente=request.POST.get('nombre_cliente')
+        consecutivo=request.POST.get('consec')
+        codigo_producto1=request.POST.get('codigo_producto')
+        codigo_producto2=request.POST.get('codigo_producto2')
+        codigo_producto3=request.POST.get('codigo_producto3')
+        nombre_producto1=request.POST.get('nombre_producto')
+        nombre_producto2=request.POST.get('nombre_producto2')
+        nombre_producto3=request.POST.get('nombre_producto3')
+        cantidad1=request.POST.get('cantidad1')
+        cantidad2=request.POST.get('cantidad2')
+        cantidad3=request.POST.get('cantidad3')
+        precio1=request.POST.get('total_venta')
+        precio2=request.POST.get('total_venta2')
+        precio3=request.POST.get('total_venta3')
+        total_compra=request.POST.get('total_venta')
+        iva=request.POST.get('ivaventa')
+        total_venta=request.POSt.get('valor_venta')
+    except Exception:
+        print('algo salio mal')
