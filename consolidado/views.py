92971c6c29687d2cbdae2b8112d4aa941cf92068
diego150104 <diegofernando150104@gmail.com>
2021-12-03 10:29:36 -0500

    cambios

--- consolidado/views.py
+++ consolidado/views.py
@@ -1,5 +1,15 @@
+from django.http import response
 from django.shortcuts import render
+import requests
 
 def consolidado(request):
-
+    responseventa= requests.get('http://localhost:8004/api/ventas/')
+    sumaventa= []
+    if responseventa.status_code==200:
+       data= responseventa.json()
+    for i in data:
+        sumaventa.append(i['total_venta'])
+    totalventa= sum(sumaventa)
+    dataconsolidado= {'ciudad':'bogota','total_ventas':totalventa}
+    responseconsolidado= requests.post('http://localhost:8005/api/consolidado/', data=dataconsolidado)
     return render(request, "consolidado/Consolidacion.html")
